68875ac178da02b5caaa80716c1f79e7cbdca13f
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-11-12 08:59:25 -0500

    Added calculations and logic in OrderForm

--- COMP1004-F2016-Assignment3-Movie/OrderForm.cs
+++ COMP1004-F2016-Assignment3-Movie/OrderForm.cs
@@ -45,5 +45,46 @@ namespace COMP1004_F2016_Assignment3_Movie
             CalculateTotals();
         }
 
+        //Method will calculate the totals of the movie depending on whether or not the add dvd box is checked
+        private void CalculateTotals()
+        {
+
+            if (OrderDvdCheckBox.Checked == true)
+            {
+                DvdLabel.Show();
+                DvdCostTextBox.Show();
+                double TotalTax = Convert.ToDouble((CostTextBox.Text as string).TrimStart('$'));
+                double DvdCost = TotalTax + 10;
+
+
+                TotalTax = DvdCost * 0.13;
+                double Grand = TotalTax + DvdCost;
+
+
+                SubtotalTextBox.Text = DvdCost.ToString("C2");
+                SalesTaxTextBox.Text = TotalTax.ToString("C2");
+                GrandTotalTextBox.Text = Grand.ToString("C2");
+                Program.info.GrandTotal = GrandTotalTextBox.Text;
+
+            }
+            else if (OrderDvdCheckBox.Checked == false)
+            {
+                DvdLabel.Hide();
+                DvdCostTextBox.Hide();
+                CostTextBox.Text = Program.info.Cost;
+                double TotalTax = Convert.ToDouble((CostTextBox.Text as string).TrimStart('$'));
+                double Grand = TotalTax * 1.13;
+                TotalTax = TotalTax * 0.13;
+
+
+
+                SubtotalTextBox.Text = CostTextBox.Text;
+                SalesTaxTextBox.Text = TotalTax.ToString("C2");
+                GrandTotalTextBox.Text = Grand.ToString("C2");
+                Program.info.GrandTotal = GrandTotalTextBox.Text;
+            }
+
+        }
+
     }
 }
