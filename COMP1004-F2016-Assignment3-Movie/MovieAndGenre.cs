2cc8d9e287f5dc0cca2038484e5cb272ccc97c7a
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-11-12 08:55:32 -0500

    Added variables to the MovieAndGenre Form

--- COMP1004-F2016-Assignment3-Movie/MovieAndGenre.cs
+++ COMP1004-F2016-Assignment3-Movie/MovieAndGenre.cs
@@ -9,6 +9,20 @@ namespace COMP1004_F2016_Assignment3_Movie
     public class MovieAndGenre
     {
 
+        //Movie Title Info
+        public string Title { get; set; }
+
+        //Movie genre info
+        public string Genre { get; set; }
+
+        //Movie cost Info
+        public string Cost { get; set; }
+
+        //Movie Grand total info
+        public string GrandTotal { get; set; }
+
+        //Movie picture
+        public System.Drawing.Bitmap Picture { get; set; }
 
     }
 }
